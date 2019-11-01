079f9077ce71aa18450f8abde20944999fc2b35b
Feng <56996751+hisongfeng@users.noreply.github.com>
2019-11-01 11:40:37 +0800

    rtl8720c: apps: support key switch
    Signed-off-by: Yongsul Oh <46733179+YongsulOh@users.noreply.github.com>

--- apps/rtl8720c/st_switch/main/smart_switch.c
+++ apps/rtl8720c/st_switch/main/smart_switch.c
@@ -20,19 +20,37 @@
 #include "st_dev.h"
 #include "PinNames.h"
 #include <gpio_api.h>
+#include "gpio_irq_api.h"
+#include "gpio_irq_ex_api.h"
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "task.h"
 
 IOT_CTX *ctx = NULL;
-IOT_CAP_HANDLE * handle;
+IOT_CAP_HANDLE *handle = NULL;
 
 enum smartswitch_switch_onoff_state {
 	SMARTSWITCH_SWITCH_OFF = 0,
 	SMARTSWITCH_SWITCH_ON = 1,
 };
 
-#define LED_PORT PA_4
-gpio_t gpio_ctrl;
-int led_onoff = 0;
-IOT_CAP_HANDLE *handle = NULL;
+enum button_gpio_state {
+    BUTTON_GPIO_RELEASED = 0,
+    BUTTON_GPIO_PRESSED = 1,
+};
+
+static int smartswitch_switch_state = SMARTSWITCH_SWITCH_ON;
+#define BUTTON_DEBOUNCE_TIME_MS 20
+
+#define GPIO_OUTPUT_NOTIFICATION_LED PA_23
+#define GPIO_INPUT_BUTTON PA_4
+#define  BUTTON_SHORT_PRESS	1
+
+gpio_t gpio_ctrl_noti;
+gpio_t gpio_ctrl_button;
+
+static xQueueHandle button_event_queue = NULL;
+
 // onboarding_config_start is null-terminated string
 extern const uint8_t onboarding_config_start[]	asm("_binary_onboarding_config_json_start");
 extern const uint8_t onboarding_config_end[]	asm("_binary_onboarding_config_json_end");
@@ -43,15 +61,15 @@ extern const uint8_t device_info_end[]		asm("_binary_device_info_json_end");
 extern unsigned int _binary_device_info_json_size;
 
 
-int led_status()
+int led_status(void)
 {
-	return led_onoff;
+	return smartswitch_switch_state;
 }
 
 static void led_state_switch(enum smartswitch_switch_onoff_state state)
 {
-	gpio_write(&gpio_ctrl, state);
-	led_onoff = state;
+	gpio_write(&gpio_ctrl_noti, state);
+	smartswitch_switch_state = state;
 }
 
 void led_switch(int on)
@@ -59,7 +77,6 @@ void led_switch(int on)
 	enum smartswitch_switch_onoff_state state = on ? SMARTSWITCH_SWITCH_ON: SMARTSWITCH_SWITCH_OFF;
 	const char * switch_str = on ? "on" : "off";
 	IOT_EVENT *switch_evt;
-	uint8_t evt_num = 1;
 	int32_t sequence_no;
 
 	/* Setup switch state */
@@ -76,13 +93,25 @@ void led_switch(int on)
 	}
 }
 
-void led_init()
+static void button_event(IOT_CAP_HANDLE *handle, uint32_t type, uint32_t count)
 {
-	gpio_init(&gpio_ctrl, LED_PORT);
-	gpio_mode(&gpio_ctrl, PullNone);
-	gpio_dir(&gpio_ctrl, PIN_OUTPUT);
-	gpio_write(&gpio_ctrl, 1);
+	if (type == BUTTON_SHORT_PRESS) {
+		printf("Button short press, count: %d\r\n", count);
+		switch(count) {
+			case 1:
+				/* change switch state and LED state */
+				if (smartswitch_switch_state == SMARTSWITCH_SWITCH_ON) {
+					led_state_switch(SMARTSWITCH_SWITCH_OFF);
+					led_switch(0);
+				} else {
 	led_state_switch(SMARTSWITCH_SWITCH_ON);
+					led_switch(1);
+				}
+				break;
+			default:
+				break;
+		}
+	}
 }
 
 static void iot_status_cb(iot_status_t status,
@@ -103,6 +132,52 @@ static void iot_status_cb(iot_status_t status,
 			printf("come here \r\n");
 			break;
 	}
+}
+
+void button_isr_handler(void *arg)
+{
+	static uint32_t last_time_ms = 0;
+	uint32_t now_ms = xTaskGetTickCountFromISR() * portTICK_PERIOD_MS;
+
+	/* check debounce time to ignore small ripple of currunt */
+	if (now_ms - last_time_ms > BUTTON_DEBOUNCE_TIME_MS) {
+		last_time_ms = now_ms;
+		xQueueSendFromISR(button_event_queue, &now_ms, NULL);
+	}
+}
+
+bool get_button_event(int* button_event_type, int* button_event_count)
+{
+	uint32_t button_time_ms = 0;
+	uint32_t gpio_value = 0;
+
+	if (xQueueReceive(button_event_queue, &button_time_ms, 0)) {
+		vTaskDelay(BUTTON_DEBOUNCE_TIME_MS / portTICK_PERIOD_MS);
+		gpio_value = gpio_read(&gpio_ctrl_button);
+		if (gpio_value == BUTTON_GPIO_PRESSED){
+			*button_event_type = BUTTON_SHORT_PRESS;
+			*button_event_count = 1;
+			return true;
+		}
+	}
+	return false;
+}
+
+void led_button_init(void)
+{
+	//notify led init
+	gpio_init(&gpio_ctrl_noti, GPIO_OUTPUT_NOTIFICATION_LED);
+	gpio_mode(&gpio_ctrl_noti, PullNone);
+	gpio_dir(&gpio_ctrl_noti, PIN_OUTPUT);
+	gpio_write(&gpio_ctrl_noti, 1);
+
+	//button init
+	button_event_queue = xQueueCreate(10, sizeof(uint32_t));
+	gpio_irq_init(&gpio_ctrl_button, GPIO_INPUT_BUTTON, button_isr_handler, (uint32_t)(&gpio_ctrl_button));
+	gpio_mode(&gpio_ctrl_button, PullUp);
+	gpio_irq_set(&gpio_ctrl_button, IRQ_LOW, 1);   // LOW Trigger
+	gpio_irq_enable(&gpio_ctrl_button);
+
 }
 
 void cap_switch_init_cb(IOT_CAP_HANDLE *handle, void *usr_data)
@@ -136,7 +211,7 @@ void cap_switch_cmd_on_cb(IOT_CAP_HANDLE *handle,
 	led_switch(1);
 }
 
-void cap_switch_noti_cb(iot_noti_data_t *noti_data, void *noti_usr_data)
+void iot_noti_cb(iot_noti_data_t *noti_data, void *noti_usr_data)
 {
 	printf("Notification message received\r\n");
 
@@ -145,6 +220,22 @@ void cap_switch_noti_cb(iot_noti_data_t *noti_data, void *noti_usr_data)
 	} else if (noti_data->type == IOT_NOTI_TYPE_RATE_LIMIT) {
 		printf("[rate limit] Remaining time:%d, sequence number:%d\r\n",
 			noti_data->raw.rate_limit.remainingTime, noti_data->raw.rate_limit.sequenceNumber);
+	}
+}
+
+static void smartswitch_task(void *arg)
+{
+	IOT_CAP_HANDLE *handle = (IOT_CAP_HANDLE *)arg;
+
+	int button_event_type;
+	int button_event_count;
+
+	for (;;) {
+		if (get_button_event(&button_event_type, &button_event_count)) {
+			button_event(handle, button_event_type, button_event_count);
+		}
+
+		vTaskDelay(10 / portTICK_PERIOD_MS);
 	}
 }
 
@@ -181,7 +272,7 @@ void app_main(void)
 	// 1. create a iot context
 	ctx = st_conn_init(onboarding_config, onboarding_config_len, device_info, device_info_len);
 	if (ctx != NULL) {
-		iot_err = st_conn_set_noti_cb(ctx, cap_switch_noti_cb, NULL);
+		iot_err = st_conn_set_noti_cb(ctx, iot_noti_cb, NULL);
 		if (iot_err)
 			printf("fail to set notification callback function\r\n");
 
@@ -204,10 +295,10 @@ void app_main(void)
 		printf("fail to create the iot_context\r\n");
 	}
 
-	led_init();
+	led_button_init();
 
 	// 4. needed when it is necessary to keep monitoring the device status
-	//xTaskCreate(smartswitch_task, "smartswitch_task", 2048, (void *)handle, 10, NULL);
+	xTaskCreate(smartswitch_task, "smartswitch_task", 2048, (void *)handle, 10, NULL);
 
 	// 5. process on-boarding procedure. There is nothing more to do on the app side than call the API.
 	st_conn_start(ctx, (st_status_cb)&iot_status_cb, IOT_STATUS_ALL, NULL, NULL);
