b9a34ccd797f310278111b1b7911d6d9f7e20669
Hyundo Lee <54927573+hdlee27@users.noreply.github.com>
2019-12-12 09:40:46 +0900

    Modify build script for output generate
    Signed-off-by: Hyundo Lee <54927573+hdlee27@users.noreply.github.com>
    Signed-off-by: Yongsul Oh <46733179+YongsulOh@users.noreply.github.com>

--- tools/rtl8195/build_rtl8195.sh
+++ tools/rtl8195/build_rtl8195.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-CHIP_NAME=${1}
+export CHIP_NAME=${1}
 PROJECT="${PWD}/bsp/${CHIP_NAME}/project/realtek_ameba1_va0_example"
-PROJECT_TITLE=${2}
-STDK_PATH="${PWD}"
+export PROJECT_TITLE=${2}
+
+export STDK_PATH="${PWD}"
+export CORE_PATH="${PWD}/iot-core"
+
 IOT_APPS_PATH="${PWD}/apps/${CHIP_NAME}"
 IOT_OUTPUT_PATH="${PWD}/output/${CHIP_NAME}"
 OUTPUT_BUILD=y
@@ -27,54 +30,6 @@ if [ ! `command -v gawk` ];then
 	exit
 fi
 
-generate_output() {
-
-	RELEASE_DATE=`date +%Y%m%d`
-
-	if [ -e ${STDK_PATH}/.git ]; then
-		pushd ${COMPONENT_PATH} > /dev/null
-		BUILD_ID_STDK=`git log --pretty=format:'%h' -n 1`
-		popd > /dev/null
-	else
-		BUILD_ID_STDK=`date +%H%M`
-	fi
-
-	if [ -e ${COMPONENT_PATH}/.git ]; then
-		pushd ${COMPONENT_PATH} > /dev/null
-		BUILD_ID_CORE=`git log --pretty=format:'%h' -n 1`
-		popd > /dev/null
-	else
-		BUILD_ID_CORE=`date +%H%M`
-	fi
-
-	BUILD_ID=${BUILD_ID_STDK}_${BUILD_ID_CORE}
-
-	OUTPUT_PATH=${IOT_OUTPUT_PATH}/iotcore_${PROJECT_TITLE}_${RELEASE_DATE}_${BUILD_ID}
-	if [ ${RELEASE_CONFIG} ]; then
-		OUTPUT_PATH=${OUTPUT_PATH}_RELEASE
-	elif [ ${DEBUG_CONFIG} ]; then
-		OUTPUT_PATH=${OUTPUT_PATH}_DEBUG
-	fi
-
-	if [ ! -d ${OUTPUT_PATH} ]; then
-		mkdir -p ${OUTPUT_PATH}
-	else
-		rm -rf ${OUTPUT_PATH}/*
-	fi
-
-	BINARY_PATH=${PROJECT}/GCC-RELEASE/application/Debug/bin
-	cp -f ${BINARY_PATH}/sdram.bin ${OUTPUT_PATH}/
-	cp -f ${BINARY_PATH}/ram_all.bin ${OUTPUT_PATH}/
-	cp -f ${BINARY_PATH}/ram_2.bin ${OUTPUT_PATH}/
-	cp -f ${BINARY_PATH}/ota.bin ${OUTPUT_PATH}/
-
-	mkdir ${OUTPUT_PATH}/debug
-	cp -f ${BINARY_PATH}/application.map ${OUTPUT_PATH}/debug/
-	echo "-------------------------------------------------"
-	echo "binary path: ${OUTPUT_PATH}"
-	echo "-------------------------------------------------"
-}
-
 if [ "${PROJECT_TITLE}" = "" ]; then
 	print_usage
 	exit
@@ -96,5 +51,8 @@ make IMAGE_NAME=dnakit-st $* 2>&1 | tee build.log
 
 popd > /dev/null
 if [ ${OUTPUT_BUILD} = y ]; then
-	generate_output
+	BINARY_PATH=${PROJECT}/GCC-RELEASE/application/Debug/bin
+	export OUTPUT_FILE_LIST="${BINARY_PATH}/sdram.bin ${BINARY_PATH}/ram_all.bin ${BINARY_PATH}/ram_2.bin ${BINARY_PATH}/ota.bin"
+	export DEBUG_FILE_LIST="${BINARY_PATH}/application.map"
+	${STDK_PATH}/tools/common/generate_output.sh
 fi
