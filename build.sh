c41a5b64b90ad0443b7cf78a7d4fba3854200dc8
Hyundo Lee <54927573+hdlee27@users.noreply.github.com>
2020-07-10 14:31:05 +0900

    modify build.sh to support path argument and error message
    Signed-off-by: Hyundo Lee <54927573+hdlee27@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -1,31 +1,56 @@
 #!/bin/bash
 
-CHIP_LIST=("esp8266" "esp32" "rtl8195" "rtl8720c" "rtl8721c")
-
 CHIP_NAME=${1}
 APP_NAME=${2}
+ARGUMENTS=$@
+
+if [[ "${1}" == "apps/"*"/"?* ]]; then
+    CHIP_NAME=`echo ${1} | awk -F'/' '{print $2}'`
+    APP_NAME=`echo ${1} | awk -F'/' '{print $3}'`
+    shift 1
+    ARGUMENTS="${CHIP_NAME} ${APP_NAME} $@"
+fi
 
 print_usage () {
-	echo "    Usage: ./build.sh CHIP_NAME APP_NAME"
-	echo "- - - - - - - - - - - - - - - - - - -"
-	echo "    ex) ./build.sh esp8266 st_switch"
-	echo "    ex) ./build.sh esp8266 st_lamp"
-	echo "    ex) ./build.sh esp32 st_switch"
-	echo "    ex) ./build.sh rtl8195 st_switch"
-	echo "    ex) ./build.sh rtl8720c st_switch"
-	echo "    ex) ./build.sh rtl8721c st_switch"
-	echo
+    echo "    Usage: ./build.sh CHIP_NAME APP_NAME"
+    echo "                     or "
+    echo "           ./build.sh apps/CHIP_NAME/APP_NAME"
+    echo "- - - - - - - - - - - - - - - - - - -"
+    echo "    ex) ./build.sh apps/esp8266/st_switch"
+    echo "    ex) ./build.sh apps/esp32/st_switch"
+    echo "    ex) ./build.sh rtl8195 st_switch"
+    echo "    ex) ./build.sh rtl8720c st_switch"
+    echo "    ex) ./build.sh rtl8721c st_switch"
+    echo
+}
+
+print_path() {
+    if [ "${CHIP_NAME}" == "" -o ! -d apps/${CHIP_NAME} ]; then
+        echo "Fail to find CHIP_NAME (apps/${CHIP_NAME}) "
+        echo "ls apps/"
+        ls apps/
+    elif [ "${APP_NAME}" == "" -o ! -d apps/${CHIP_NAME}/${APP_NAME} ]; then
+        echo "Fail to find APP_NAME (apps/${CHIP_NAME}/${APP_NAME})"
+        echo "ls apps/${CHIP_NAME}/"
+        ls apps/${CHIP_NAME}
+    fi
+    echo
 }
 
 if [ "${CHIP_NAME}" == "" ]; then
-	print_usage
-	exit 0
+    print_usage
+    print_path
+    exit 0
 fi
 
 if [ ! -e tools/${CHIP_NAME}/build_${CHIP_NAME}.sh ]; then
-	echo "Failed to find tools/${CHIP_NAME}/build_${CHIP_NAME}.sh"
-	print_usage
-	exit 1
+    echo "Failed to find tools/${CHIP_NAME}/build_${CHIP_NAME}.sh"
+    print_usage
+    print_path
+    exit 1
 fi
 
-tools/${CHIP_NAME}/build_${CHIP_NAME}.sh $@
+tools/${CHIP_NAME}/build_${CHIP_NAME}.sh ${ARGUMENTS}
+if [ "$?" -ne "0" ]; then
+    print_path
+fi
