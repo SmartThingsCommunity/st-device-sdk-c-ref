b473f41cfa2754dd11725ca07ae528ec5d234cfb
Hyundo Lee <54927573+hdlee27@users.noreply.github.com>
2020-08-05 13:09:03 +0900

    update capability sample files for attribute logic
    Change-Id: I6475310f43f80c9f20fa9e40ed59c8d96c2988dc
    Signed-off-by: Hyundo Lee <54927573+hdlee27@users.noreply.github.com>

--- apps/esp32/light_example/main/caps_switchLevel.c
+++ apps/esp32/light_example/main/caps_switchLevel.c
@@ -61,27 +61,24 @@ static void caps_switchLevel_set_level_unit(caps_switchLevel_data_t *caps_data, 
 
 static void caps_switchLevel_attr_level_send(caps_switchLevel_data_t *caps_data)
 {
-    IOT_EVENT *cap_evt;
-    uint8_t evt_num = 1;
-    int sequence_no;
+    int sequence_no = -1;
 
     if (!caps_data || !caps_data->handle) {
         printf("fail to get handle\n");
         return;
     }
 
-    cap_evt = st_cap_attr_create_int((char *) caps_helper_switchLevel.attr_level.name, caps_data->level_value, caps_data->level_unit);
-    if (!cap_evt) {
-        printf("fail to create cap_evt\n");
-        return;
-    }
+    ST_CAP_SEND_ATTR_NUMBER(caps_data->handle,
+            (char *)caps_helper_switchLevel.attr_level.name,
+            caps_data->level_value,
+            caps_data->level_unit,
+            NULL,
+            sequence_no);
 
-    sequence_no = st_cap_attr_send(caps_data->handle, evt_num, &cap_evt);
     if (sequence_no < 0)
         printf("fail to send level value\n");
-
-    printf("Sequence number return : %d\n", sequence_no);
-    st_cap_attr_free(cap_evt);
+    else
+        printf("Sequence number return : %d\n", sequence_no);
 }
 
 
