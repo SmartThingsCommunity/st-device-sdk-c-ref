f6a7ed251b4e43dcb75c2c56e8ba0c6ac1b626ff
Hyundo Lee <54927573+hdlee27@users.noreply.github.com>
2020-08-14 13:14:35 +0900

    add build/setup python script for esp32_v4.x
    Signed-off-by: Hyundo Lee <54927573+hdlee27@users.noreply.github.com>

---
+++ tools/esp32_v4.x/build_esp32_v4.x.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import platform
+import sys
+import os
+import shutil
+import subprocess
+
+OUTPUT_OPTION_ARRAY=["all", "flash", "app", "app-flash", "bootloader", "bootloader-flash", "partition_table"]
+
+BSP_NAME = sys.argv[1]
+APP_NAME = sys.argv[2]
+EXTRA_ARGS = sys.argv[3:]
+
+BSP_PATH = os.path.join(os.environ["STDK_REF_PATH"], "bsp", BSP_NAME)
+APP_PATH = os.path.join(os.environ["STDK_REF_PATH"], "apps", "esp32", APP_NAME)
+
+STDK_PATH = os.environ["STDK_REF_PATH"]
+CORE_PATH = os.environ["STDK_CORE_PATH"]
+
+os.environ["IDF_PATH"] = BSP_PATH
+
+print("PATH : " + os.environ["PATH"])
+
+if not (os.path.exists(BSP_PATH) and os.path.exists(APP_PATH)):
+  print("Fail to find path.")
+  exit(1)
+
+
+shutil.copy(os.path.join(CORE_PATH, "src", "Kconfig"), os.path.join(BSP_PATH, "components", "iot-core_config"))
+
+if EXTRA_ARGS:
+    MAKE_OPTION = ""
+    for args in EXTRA_ARGS:
+        MAKE_OPTION = MAKE_OPTION + " " + args
+else:
+    MAKE_OPTION = "build"
+
+os.chdir(BSP_PATH)
+if platform.system() == "Windows":
+    export_cmd = "export.bat"
+else:
+    export_cmd = ". ./export.sh"
+build_cmd = "python3 " + os.path.join(BSP_PATH, "tools", "idf.py") + " " + MAKE_OPTION
+
+subprocess.call(export_cmd + " && cd " + APP_PATH + " && "+ build_cmd, shell=True)
---
+++ tools/esp32_v4.x/setup_esp32_v4.x.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import platform
+import sys
+import os
+import shutil
+import subprocess
+
+BSP_NAME = sys.argv[1]
+BSP_PATH = os.path.join(os.environ["STDK_REF_PATH"], "bsp", BSP_NAME)
+PATCH_PATH = os.path.join(os.environ["STDK_REF_PATH"], "patches", BSP_NAME)
+
+CONFIG_PATH = os.path.join(BSP_PATH, "components", "iot-core_config")
+
+ret_val = os.system("git submodule status " + BSP_PATH + " &> /dev/null")
+if not ret_val:
+	os.chdir(BSP_PATH)
+	os.system("git am " + os.path.join(PATCH_PATH, "*.patch"))
+	if not os.path.exists(CONFIG_PATH):
+		os.makedirs(CONFIG_PATH)
+
+	cmake_file = open(os.path.join(CONFIG_PATH, "CMakeLists.txt"), "w")
+	cmake_file.write("idf_component_register()")
+	cmake_file.close()
+	exit(0)
+elif platform.system() == "Windows":
+	print("Failed to apply patches")
+	exit(1)
+else:
+	if os.listdir(BSP_PATH) == []:
+		print("Failed to find source code in " + BSP_PATH)
+		exit(1)
+	else:
+		os.chdir(BSP_PATH)
+		patch_files = os.listdir(PATCH_PATH)
+		for patch_file_name in patch_files:
+			os.system("patch -f -p1 < " + os.path.join(PATCH_PATH, patch_file_name))
+		print("Check source code in " + BSP_PATH)
+		exit(0)
+
+os.chdir(BSP_PATH)
+if platform.system() == "Windows":
+	os.system("install.bat")
+else:
+	os.system("./install.sh")
