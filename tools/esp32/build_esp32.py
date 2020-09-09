1e52c37fd0c2f908c92309f9a1a9ee9573e54d1a
Hyundo Lee <54927573+hdlee27@users.noreply.github.com>
2020-09-09 11:49:50 +0900

    modify esp32 build script to make qrcode and check submodule
    Change-Id: I8357067768f532de621c9f7277a83147586c8cb6
    Signed-off-by: Hyundo Lee <54927573+hdlee27@users.noreply.github.com>

--- tools/esp32/build_esp32.py
+++ tools/esp32/build_esp32.py
@@ -6,6 +6,20 @@ import os
 import shutil
 import subprocess
 
+def is_valid_submodule(target_path):
+    working_dir = os.getcwd()
+    os.chdir(os.path.dirname(working_dir))
+    target_commit_id = subprocess.check_output(['git','ls-files','-s', target_path]).split(' ')[1][0:7]
+    os.chdir(target_path)
+    number_of_patches = len(subprocess.check_output(['git','log', '--branches', '--not', '--remotes', '--pretty=oneline']).splitlines())
+    target_commit_list = subprocess.check_output(['git', 'log', '-n'+str(number_of_patches+1), '--pretty=oneline']).splitlines()
+    matched_line = [line for line in target_commit_list if line.startswith(target_commit_id)]
+    os.chdir(working_dir)
+    if matched_line:
+        return 1
+    else:
+        return 0
+
 OUTPUT_OPTION_ARRAY=["all", "flash", "app", "app-flash", "bootloader", "bootloader-flash", "partition_table"]
 
 BSP_NAME = sys.argv[1]
@@ -14,9 +28,7 @@ EXTRA_ARGS = sys.argv[3:]
 
 BSP_PATH = os.path.join(os.environ["STDK_REF_PATH"], "bsp", BSP_NAME)
 APP_PATH = os.path.join(os.environ["STDK_REF_PATH"], "apps", "esp32", APP_NAME)
-
-STDK_PATH = os.environ["STDK_REF_PATH"]
-CORE_PATH = os.environ["STDK_CORE_PATH"]
+COMMON_TOOLS_PATH = os.path.join(os.environ["STDK_REF_PATH"], "tools", "common")
 
 os.environ["IDF_PATH"] = BSP_PATH
 
@@ -24,7 +36,7 @@ if not (os.path.exists(BSP_PATH) and os.path.exists(APP_PATH)):
     print("Fail to find path.")
     exit(1)
 
-shutil.copy(os.path.join(CORE_PATH, "src", "Kconfig"), os.path.join(BSP_PATH, "components", "iot-core_config", "Kconfig"))
+shutil.copy(os.path.join(os.environ["STDK_CORE_PATH"], "src", "Kconfig"), os.path.join(BSP_PATH, "components", "iot-core_config", "Kconfig"))
 
 if EXTRA_ARGS:
     MAKE_OPTION = ""
@@ -42,3 +54,53 @@ else:
 build_cmd = "python " + os.path.join(BSP_PATH, "tools", "idf.py") + " " + MAKE_OPTION
 
 subprocess.call(export_cmd + " && cd " + APP_PATH + " && " + build_cmd, shell=True)
+
+#generate output
+with open(os.path.join(APP_PATH, "CMakeLists.txt"), 'r') as readfile:
+    lines = readfile.readlines()
+    for line in lines:
+        if line.startswith("project"):
+            project_name= line.replace(')','(').split('(')[1]
+
+use_custom_partition_table = False
+with open(os.path.join(APP_PATH, "sdkconfig"), 'r') as readfile:
+     lines = readfile.readlines()
+     for line in lines:
+         if line.startswith("CONFIG_PARTITION_TABLE_CUSTOM=y"):
+             use_custom_partition_table = True
+         elif line.startswith("CONFIG_PARTITION_TABLE_CUSTOM_FILENAME="):
+             custom_partition_name = line.replace('"','=').split('=')[2]
+         elif line.startswith("CONFIG_PARTITION_TABLE_FILENAME="):
+             partition_name = line.replace('"','=').split('=')[2]
+if use_custom_partition_table:
+    partition_name = custom_partition_name
+
+qr_file =  os.path.join(APP_PATH, project_name + ".png")
+subprocess.call("python " + os.path.join(COMMON_TOOLS_PATH, "generate_qr.py") + " " + APP_PATH + " " + qr_file, shell=True)
+
+output_list = [
+    os.path.join(APP_PATH, "build", "ota_data_initial.bin"),
+    os.path.join(APP_PATH, "build", "bootloader", "bootloader.bin"),
+    os.path.join(APP_PATH, "build", project_name + ".bin"),
+    os.path.join(APP_PATH, "build", "partition_table", "partition-table.bin"),
+]
+
+if os.path.isfile(qr_file):
+    output_list.append(qr_file)
+
+debug_list = [
+    os.path.join(APP_PATH, "build", project_name + ".elf"),
+    os.path.join(APP_PATH, "build", project_name + ".map"),
+    os.path.join(APP_PATH, "build", "bootloader", "bootloader.elf"),
+    os.path.join(APP_PATH, "build", "bootloader", "bootloader.map"),
+    os.path.join(APP_PATH, "sdkconfig"),
+    ]
+
+sys.path.append(COMMON_TOOLS_PATH)
+from generate_output import generate_output
+generate_output(os.environ["STDK_REF_PATH"], BSP_NAME, APP_NAME, output_list, debug_list)
+
+if os.path.isfile(qr_file):
+    os.remove(qr_file)
+
+subprocess.call("python " + os.path.join(COMMON_TOOLS_PATH, "check_submodule.py") + " " + BSP_PATH, shell=True)
--- tools/esp32/build_esp32.sh
+++ tools/esp32/build_esp32.sh
@@ -8,11 +8,12 @@ export CORE_PATH="${PWD}/iot-core"
 
 IOT_APPS_PATH="${PWD}/apps/${BSP_NAME}"
 PROJECT_PATH="${IOT_APPS_PATH}/${PROJECT_TITLE}"
+COMMON_TOOLS_PATH="${STDK_PATH}/tools/common"
 
 export IDF_PATH="${PWD}/bsp/${BSP_NAME}"
 
 MAKE_OPTION_ARRAY=("menuconfig" "defconfig" "all" "flash" "clean" "size" "size-components" "size-symbols" "erase_flash" "monitor" "simple_monitor" "list-components" "app" "app-flash" "app-clean" "print_flash_cmd" "help" "bootloader" "bootloader-flash" "bootloader-clean" "partition_table")
-OUTPUT_OPTION_ARRAY=("all" "flash" "app" "app-flash" "bootloader" "bootloader-flash" "partition_table")
+OUTPUT_OPTION_ARRAY=("build" "all" "flash" "app" "app-flash" "bootloader" "bootloader-flash" "partition_table")
 
 MAKE_OPTION=build
 
@@ -70,32 +71,22 @@ fi
 ### Write address_info.txt
 PROJECT_NAME=`cat ./Makefile | grep ^PROJECT_NAME | awk '{print $3}'`
 
-if [ "`cat ./sdkconfig | grep ^CONFIG_PARTITION_TABLE_CUSTOM\= | awk -F'=' '{print $2}'`" = "y" ]; then
-  PARTITION_NAME=`cat ./sdkconfig | grep ^CONFIG_PARTITION_TABLE_CUSTOM_FILENAME | awk -F'="' '{print $2}'`
-  PARTITION_TABLE=${PROJECT_PATH}
-else
-  PARTITION_NAME=`cat ./sdkconfig | grep ^CONFIG_PARTITION_TABLE_FILENAME | awk -F'="' '{print $2}'`
-  PARTITION_TABLE=${IDF_PATH}/components/partition_table
+QR_FILE="${PROJECT_PATH}/${PROJECT_TITLE}.png"
+${COMMON_TOOLS_PATH}/generate_qr.py ${PROJECT_PATH} ${QR_FILE}
+if [ ! -f ${QR_FILE} ]; then
+    QR_FILE=""
 fi
-PARTITION_NAME=${PARTITION_NAME%.*}
-PARTITION_TABLE=${PARTITION_TABLE}/${PARTITION_NAME}.csv
-
-GET_PART_INFO="${STDK_PATH}/bsp/${BSP_NAME}/components/partition_table/parttool.py -q"
-
-BOOTLOADER_OFFSET=`cat ${IDF_PATH}/components/bootloader/Makefile.projbuild | grep  -E "BOOTLOADER_OFFSET" | awk -F ':= ' '{print $2}'`
-APP_OFFSET=`${GET_PART_INFO} --partition-boot-default --partition-table-file ${PROJECT_PATH}/build/${PARTITION_NAME}.bin get_partition_info --info offset`
-OTA_DATA_OFFSET=`${GET_PART_INFO} --partition-type data --partition-subtype ota --partition-table-file ${PROJECT_PATH}/build/${PARTITION_NAME}.bin get_partition_info --info offset`
-PARTITION_OFFSET=`cat ${PROJECT_PATH}/sdkconfig | grep ^CONFIG_PARTITION_TABLE_OFFSET\= | awk -F'=' '{print $2}'`
-
-ADDRESS_INFO_FILE=${PROJECT_PATH}/address_info.txt
-echo ota_data_initial.bin : ${OTA_DATA_OFFSET} > ${ADDRESS_INFO_FILE}
-echo bootloader.bin : ${BOOTLOADER_OFFSET} >> ${ADDRESS_INFO_FILE}
-echo ${PROJECT_NAME}.bin : ${APP_OFFSET} >> ${ADDRESS_INFO_FILE}
-echo ${PARTITION_NAME}.bin : ${PARTITION_OFFSET} >> ${ADDRESS_INFO_FILE}
 
 ### Generate output
-export OUTPUT_FILE_LIST="${PROJECT_PATH}/build/ota_data_initial.bin ${PROJECT_PATH}/build/bootloader/bootloader.bin ${PROJECT_PATH}/build/${PROJECT_NAME}.bin ${PROJECT_PATH}/build/${PARTITION_NAME}.bin ${ADDRESS_INFO_FILE}"
+export OUTPUT_FILE_LIST="${PROJECT_PATH}/build/ota_data_initial.bin ${PROJECT_PATH}/build/bootloader/bootloader.bin ${PROJECT_PATH}/build/${PROJECT_NAME}.bin ${PROJECT_PATH}/build/partition_table/partition-table.bin ${QR_FILE}"
 
 export DEBUG_FILE_LIST="${PROJECT_PATH}/build/${PROJECT_NAME}.elf ${PROJECT_PATH}/build/${PROJECT_NAME}.map ${PROJECT_PATH}/build/bootloader/bootloader.elf ${PROJECT_PATH}/build/bootloader/bootloader.map ${PROJECT_PATH}/sdkconfig"
 
 ${STDK_PATH}/tools/common/generate_output.sh
+
+if [ ${QR_FILE} ]; then
+    rm ${QR_FILE}
+fi
+
+python ${COMMON_TOOLS_PATH}/check_submodule.py ${IDF_PATH}
+
