e0e7c85fc44ff9206f7bdd145fd4542ae546c0d7
Hyundo Lee <54927573+hdlee27@users.noreply.github.com>
2020-09-15 16:02:45 +0900

    modify to decide whether to copy output by refferring build option
    Change-Id: I93645b8e09d6afb8b0ef7eb55f9ab6d797332d6a
    Signed-off-by: Hyundo Lee <54927573+hdlee27@users.noreply.github.com>

--- tools/esp32/build_esp32.py
+++ tools/esp32/build_esp32.py
@@ -6,7 +6,7 @@ import os
 import shutil
 import subprocess
 
-OUTPUT_OPTION_ARRAY=["all", "flash", "app", "app-flash", "bootloader", "bootloader-flash", "partition_table"]
+OUTPUT_OPTION_ARRAY=["all", "build", "flash", "app", "app-flash", "bootloader", "bootloader-flash", "partition_table"]
 AFTER_OPTION_ARRAY=["flash", "monitor"]
 
 BSP_NAME = sys.argv[1]
@@ -47,6 +47,51 @@ def get_qrgen_image():
                     qrgen_image = os.path.abspath(qrgen_image)
     return qrgen_image
 
+def copy_output():
+    # generate output
+    with open(os.path.join(APP_PATH, "CMakeLists.txt"), 'r') as readfile:
+        lines = readfile.readlines()
+        for line in lines:
+            if line.startswith("project"):
+                project_name = line.replace(')', '(').split('(')[1]
+
+    use_custom_partition_table = False
+    with open(os.path.join(APP_PATH, "sdkconfig"), 'r') as readfile:
+        lines = readfile.readlines()
+        for line in lines:
+            if line.startswith("CONFIG_PARTITION_TABLE_CUSTOM=y"):
+                use_custom_partition_table = True
+            elif line.startswith("CONFIG_PARTITION_TABLE_CUSTOM_FILENAME="):
+                custom_partition_name = line.replace('"', '=').split('=')[2]
+            elif line.startswith("CONFIG_PARTITION_TABLE_FILENAME="):
+                partition_name = line.replace('"', '=').split('=')[2]
+    if use_custom_partition_table:
+        partition_name = custom_partition_name
+
+    output_list = [
+        os.path.join(APP_PATH, "build", "ota_data_initial.bin"),
+        os.path.join(APP_PATH, "build", "bootloader", "bootloader.bin"),
+        os.path.join(APP_PATH, "build", project_name + ".bin"),
+        os.path.join(APP_PATH, "build", "partition_table", "partition-table.bin"),
+    ]
+
+    qrgen_image = get_qrgen_image()
+    if qrgen_image:
+        output_list.append(os.path.join(qrgen_image))
+
+    debug_list = [
+        os.path.join(APP_PATH, "build", project_name + ".elf"),
+        os.path.join(APP_PATH, "build", project_name + ".map"),
+        os.path.join(APP_PATH, "build", "bootloader", "bootloader.elf"),
+        os.path.join(APP_PATH, "build", "bootloader", "bootloader.map"),
+        os.path.join(APP_PATH, "sdkconfig"),
+    ]
+
+    sys.path.append(COMMON_TOOLS_PATH)
+    from generate_output import generate_output
+    generate_output(os.environ["STDK_REF_PATH"], BSP_NAME, APP_NAME, output_list, debug_list)
+
+
 if not (os.path.exists(BSP_PATH) and os.path.exists(APP_PATH)):
     print("Fail to find path.")
     exit(1)
@@ -70,48 +115,7 @@ build_cmd = "python " + os.path.join(BSP_PATH, "tools", "idf.py") + " " + MAKE_O
 
 subprocess.call(export_cmd + " && cd " + APP_PATH + " && " + build_cmd, shell=True)
 
-#generate output
-with open(os.path.join(APP_PATH, "CMakeLists.txt"), 'r') as readfile:
-    lines = readfile.readlines()
-    for line in lines:
-        if line.startswith("project"):
-            project_name= line.replace(')','(').split('(')[1]
-
-use_custom_partition_table = False
-with open(os.path.join(APP_PATH, "sdkconfig"), 'r') as readfile:
-     lines = readfile.readlines()
-     for line in lines:
-         if line.startswith("CONFIG_PARTITION_TABLE_CUSTOM=y"):
-             use_custom_partition_table = True
-         elif line.startswith("CONFIG_PARTITION_TABLE_CUSTOM_FILENAME="):
-             custom_partition_name = line.replace('"','=').split('=')[2]
-         elif line.startswith("CONFIG_PARTITION_TABLE_FILENAME="):
-             partition_name = line.replace('"','=').split('=')[2]
-if use_custom_partition_table:
-    partition_name = custom_partition_name
-
-
-output_list = [
-    os.path.join(APP_PATH, "build", "ota_data_initial.bin"),
-    os.path.join(APP_PATH, "build", "bootloader", "bootloader.bin"),
-    os.path.join(APP_PATH, "build", project_name + ".bin"),
-    os.path.join(APP_PATH, "build", "partition_table", "partition-table.bin"),
-]
-
-qrgen_image = get_qrgen_image()
-if qrgen_image:
-    output_list.append(os.path.join(qrgen_image))
-
-debug_list = [
-    os.path.join(APP_PATH, "build", project_name + ".elf"),
-    os.path.join(APP_PATH, "build", project_name + ".map"),
-    os.path.join(APP_PATH, "build", "bootloader", "bootloader.elf"),
-    os.path.join(APP_PATH, "build", "bootloader", "bootloader.map"),
-    os.path.join(APP_PATH, "sdkconfig"),
-    ]
-
-sys.path.append(COMMON_TOOLS_PATH)
-from generate_output import generate_output
-generate_output(os.environ["STDK_REF_PATH"], BSP_NAME, APP_NAME, output_list, debug_list)
+if set(MAKE_OPTION.split()).intersection(OUTPUT_OPTION_ARRAY):
+    copy_output()
 
 subprocess.call("python " + os.path.join(COMMON_TOOLS_PATH, "check_submodule.py") + " " + BSP_PATH + " " + PATCH_PATH, shell=True)
