809786805a923a127b018cbf28dbeb69bf51b1c3
hong.ye <52810083+hongye-samsung@users.noreply.github.com>
2020-11-02 13:14:47 +0800

    Add light_example for rtl8720c and update README.md Add CONFIG_STDK_IOT_CORE_EASYSETUP_HTTP_USE_SOCKET_API configuration
    Signed-off-by: hong.ye <52810083+hongye-samsung@users.noreply.github.com>

---
+++ apps/rtl8720c/light_example/main/device_control.h
@@ -0,0 +1,72 @@
+/* ***************************************************************************
+ *
+ * Copyright 2019-2020 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ ****************************************************************************/
+#include <stdbool.h>
+#include <string.h>
+#include "st_dev.h"
+#include "PinNames.h"
+#include <gpio_api.h>
+#include "gpio_irq_api.h"
+#include "gpio_irq_ex_api.h"
+
+
+#define GPIO_INPUT_BUTTON      PA_19
+
+#define GPIO_OUTPUT_COLORLED_R PA_2
+#define GPIO_OUTPUT_COLORLED_G PA_3
+#define GPIO_OUTPUT_COLORLED_B PA_4
+
+#define GPIO_OUTPUT_COLORLED_0 PA_0
+
+enum switch_onoff_state {
+	SWITCH_OFF = 0,
+	SWITCH_ON = 1,
+};
+
+enum color_led_gpio_state {
+	COLOR_LED_OFF = 0,
+	COLOR_LED_ON = 1,
+};
+
+enum led_animation_mode_list {
+	LED_ANIMATION_MODE_IDLE = 0,
+	LED_ANIMATION_MODE_FAST,
+	LED_ANIMATION_MODE_SLOW,
+};
+
+enum button_gpio_state {
+	BUTTON_GPIO_RELEASED = 1,
+	BUTTON_GPIO_PRESSED = 0,
+};
+
+#define BUTTON_DEBOUNCE_TIME_MS 20
+#define BUTTON_LONG_THRESHOLD_MS 5000
+#define BUTTON_DELAY_MS 300
+
+enum button_event_type {
+	BUTTON_LONG_PRESS = 0,
+	BUTTON_SHORT_PRESS = 1,
+};
+
+void change_switch_state(int switch_state);
+void update_color_info(int color_temp);
+void change_switch_level(int level);
+void button_isr_handler(void *arg);
+int get_button_event(int* button_event_type, int* button_event_count);
+void led_blink(int switch_state, int delay, int count);
+void change_led_mode(int noti_led_mode);
+void iot_gpio_init(void);
